bf883b84408efb9aeca310d45d13347c17d74788
boruch d <dubywider@gmail.com>
2019-09-11 16:05:18 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -18,5 +18,5 @@ end
 
 
 def average_gpa_for_9th_grade
-  "SELECT MAX(gpa) FROM students WHERE grade =  9 BETWEEN 0.0001 AND 3;"
+  "SELECT MAX(gpa) FROM students WHERE grade =  9 BETWEEN 0.0001 AND ;"
 end
